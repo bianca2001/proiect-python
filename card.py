7b32d970db141a18ede165438bb60cfb1af0ddc0
bianca2001 <bianca.sima007@gmail.com>
2023-12-19 13:59:39 +0200

    added face direction for cards, flip function, getters and setters

--- card.py
+++ card.py
@@ -2,6 +2,7 @@ class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+        self.face_up = False
 
     def __str__(self):
         return f"{self.rank} of {self.suit}"
@@ -11,3 +12,21 @@ class Card:
 
     def is_black(self):
         return self.suit in ["Spades", "Clubs"]
+    
+    def flip(self):
+        self.face_up = not self.face_up
+    
+    def is_face_up(self):
+        return self.face_up
+
+    def get_suit(self):
+        return self.suit
+    
+    def get_rank(self):
+        return self.rank
+
+    def set_suit(self, suit):
+        self.suit = suit
+    
+    def set_rank(self, rank):
+        self.rank = rank
