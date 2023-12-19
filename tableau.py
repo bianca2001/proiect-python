f76c207c28a2d74c7de99310252f1e6252da177d
bianca2001 <bianca.sima007@gmail.com>
2023-12-19 14:54:41 +0200

    added function to cards from a pile to other

--- tableau.py
+++ tableau.py
@@ -1,11 +1,33 @@
-from card.py import Card
-from deck.py import Deck
+from card import Card
+from deck import Deck
 
 
 class Tableau:
-    def __init__(self):
+    def __init__(self, deck):
         self.piles = []
 
-    def add_piles(self, deck):
         for i in range(7):
             self.piles.append(deck.getCards(i+1))
+            self.piles[i][-1].flip()
+
+    def is_valid_move(self, destination_pile_number, card):
+        if not card.is_face_up():
+            return False
+
+        first_card_destination = self.piles[destination_pile_number][-1]
+
+        if first_card_destination.get_color() == card.get_color():
+            return False
+
+        return True
+
+    def move_pile_to_pile(self, source_pile_number, destination_pile_number, card):
+        if self.is_valid_move(source_pile_number, card):
+            source_pile = self.piles[source_pile_number]
+            destination_pile = self.piles[destination_pile_number]
+
+            for i in range(len(source_pile)):
+                if source_pile[i] == card:
+                    for j in range(i, len(source_pile)):
+                        destination_pile.append(source_pile.pop())
+                    break
