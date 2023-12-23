1268e117072ce3c4c015b97afbdef21b9f756388
bianca2001 <bianca.sima007@gmail.com>
2023-12-23 22:54:41 +0200

    started gui

---
+++ main.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+root = tk.Tk()
+root.title("Solitaire")
+
+def create_cards():
+    for i in range(7):
+        for j in range(i + 1):
+
+            card = tk.Canvas(root, width=50, height=80, bg="white", highlightthickness=0)
+            card.grid(row=j + 4, column=i, padx=5, pady=5)
+
+            # Draw a rectangle to represent the card
+            card.create_rectangle(2, 2, 48, 78, outline="black")
+
+            rank = "A"
+            suit = "♥" 
+            card.create_text(25, 40, text=rank, font=("Arial", 16))
+            card.create_text(25, 60, text=suit, font=("Arial", 16))
+
+# Function to create foundation piles (simple rectangles)
+def create_foundation():
+    for i in range(4):
+        foundation_pile = tk.Canvas(root, width=50, height=80, bg="white", highlightthickness=0)
+        foundation_pile.grid(row=0, column=i, padx=5, pady=5)
+
+        # Draw a rectangle for foundation pile
+        foundation_pile.create_rectangle(2, 2, 48, 78, outline="black")
+
+# Function to create stock and waste piles (simple rectangles)
+def create_stock_waste():
+    stock_pile = tk.Canvas(root, width=50, height=80, bg="white", highlightthickness=0)
+    stock_pile.grid(row=0, column=8, padx=(0, 5), pady=5, sticky="en")
+
+    # Draw a rectangle for stock pile
+    stock_pile.create_rectangle(2, 2, 48, 78, outline="black")
+
+    waste_pile = tk.Canvas(root, width=50, height=80, bg="white", highlightthickness=0)
+    waste_pile.grid(row=0, column=9, padx=5, pady=5, sticky="e")
+
+    # Draw a rectangle for waste pile
+    waste_pile.create_rectangle(2, 2, 48, 78, outline="black")
+
+# Create cards, foundation piles, stock, and waste piles
+create_cards()
+create_foundation()
+create_stock_waste()
+
+root.mainloop()
